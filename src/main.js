9d711b63ef1b4dc15af4fcc54bff57d3cd9da332
AverageZ <alexzajac@live.com>
2017-01-19 12:46:28 -0500

    [style|internal] Add tile size multiplier

--- src/main.js
+++ src/main.js
@@ -11,18 +11,19 @@ class Game extends Phaser.Game {
   constructor () {
     const width = document.documentElement.clientWidth > 1280 ? 1280 : document.documentElement.clientWidth;
     const height = document.documentElement.clientHeight > 1024 ? 1024 : document.documentElement.clientHeight;
+    const multiplier = 1;
 
     /*
     * TODO: set up dynamic creation of height/width with map
     */
-    super(640 / 2, 480 / 2, Phaser.CANVAS, 'content', null);
+    super(640 / multiplier, 480 / multiplier, Phaser.CANVAS, 'content', null);
 
     this.state.add('Boot', BootState, false);
     this.state.add('Splash', SplashState, false);
     this.state.add('Game', GameState, false);
 
     this.globalState = fromJS({
-      tileSize: 32 / 2,
+      tileSize: 32 / multiplier,
     });
 
     this.state.start('Boot');
